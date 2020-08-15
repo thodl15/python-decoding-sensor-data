0f80a74ecdffb472ef815e213a28b79476c53e23
David Thomas <thodl15@gmail.com>
2020-08-15 12:31:36 -0400

    (Update) Complete Analyze Temperature Data Module

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -4,8 +4,13 @@ from datetime import date, datetime
 # Local code imports
 from house_info import HouseInfo
 from load_data import load_sensor_data
+from temperature_info import TemperatureData
 
 # Runner script for all modules
+
+# Constants
+test_area = 1
+test_date = datetime.strptime("5/9/20", "%m/%d/%y")
 
 
 ##############################
@@ -21,17 +26,28 @@ print("Loaded records: {}".format(len(data)))
 
 # Module 2 code here:
 house_info = HouseInfo(data)
-test_area = 1
+
 recs = house_info.get_data_by_area("id", rec_area=test_area)
 
 print("\nHouse sensor records for an area {} = {}".format(test_area, len(recs)))
 
-test_date = datetime.strptime("5/9/20", "%m/%d/%y")
 recs = house_info.get_data_by_date("id", rec_date=test_date)
 
 print("\nHouse sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
 
 # Module 3 code here:
+temperature_data = TemperatureData(data)
+recs = temperature_data.get_data_by_area(rec_area = test_area)
+
+print("\nHouse Temperature sensor records for are {} = {}".format(test_area, len(recs)))
+print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
+
+recs = temperature_data.get_data_by_date(rec_date=test_date)
+
+# Step 9:
+# Missing right parenthesis in example
+print("\nHouse Temperature sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
+print("\tMaximum: {0}, Minimum: {1} temperatures". format(max(recs), min(recs)))
 
 # Module 4 code here:
 
